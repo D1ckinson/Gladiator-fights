b6882bd5ffcccfd4a268b7e80770ac65b98aa92a
Макс Крячков <123maxik321@mail.ru>
2023-10-03 20:27:12 +0300

    1.4

--- Гладиаторские бои/Program.cs
+++ Гладиаторские бои/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NPOI.SS.Formula.Functions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,12 +7,13 @@ namespace Гладиаторские_бои
 {
     internal class Program
     {
+
         static void Main()
         {
             Console.CursorVisible = false;
 
             Arena arena = new Arena();
-            Dictionary<string, Action> actions = new Dictionary<string, Action> { { "Начать бой", arena.Fight } };
+            Dictionary<string, Action> actions = new Dictionary<string, Action> { { "Начать резню", arena.Work } };
             Menu menu = new Menu(actions);
 
             menu.Work();
@@ -23,6 +25,8 @@ namespace Гладиаторские_бои
         const ConsoleKey MoveSelectionUp = ConsoleKey.UpArrow;
         const ConsoleKey MoveSelectionDown = ConsoleKey.DownArrow;
         const ConsoleKey ConfirmSelection = ConsoleKey.Enter;
+
+        private ConsoleColor[] _colorsOfSelectedItem = { ConsoleColor.Black, ConsoleColor.White };
 
         private int _index = 0;
         private bool _isRunning;
@@ -92,7 +96,10 @@ namespace Гладиаторские_бои
                     return true;
             }
 
-            _index = _index > lastIndex ? _index = lastIndex : _index < 0 ? _index = 0 : _index;
+            if (_index > lastIndex)
+                _index = lastIndex;
+            else if (_index < 0)
+                _index = 0;
 
             return false;
         }
@@ -102,16 +109,10 @@ namespace Гладиаторские_бои
             Console.SetCursorPosition(0, 0);
 
             for (int i = 0; i < _items.Length; i++)
-            {
                 if (i == _index)
-                {
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                }
-
-                Console.WriteLine(_items[i]);
-                Console.ResetColor();
-            }
+                    UserUtilities.WriteColoredText(true, _items[i], _colorsOfSelectedItem[0], _colorsOfSelectedItem[1]);
+                else
+                    Console.WriteLine(_items[i]);
         }
 
         private void Exit() => _isRunning = false;
@@ -122,6 +123,9 @@ namespace Гладиаторские_бои
         private Warrior[] _opponents = new Warrior[2];
         private Menu _menu;
         private IReadOnlyDictionary<string, Func<Warrior>> _warriorsClasses;
+
+        private int _statsPositionX = 55;
+        private int _statsPositionY = 10;
 
         public Arena()
         {
@@ -137,27 +141,25 @@ namespace Гладиаторские_бои
             _menu = new Menu(_warriorsClasses);
         }
 
-        public void Fight()
+        public void Work()
         {
             _menu.WorkArena(_opponents);
 
+            Fight();
+
+            TellFightResult();
+
+            AskPressKey();
+        }
+
+        private void Fight()
+        {
             while (_opponents[0].IsWarriorAlive() && _opponents[1].IsWarriorAlive())
             {
                 AttackOpponent(_opponents[0], _opponents[1]);
 
                 AttackOpponent(_opponents[1], _opponents[0]);
             }
-
-            if (_opponents[0].IsWarriorAlive() == false && _opponents[1].IsWarriorAlive() == false)
-                Console.WriteLine("Войны не смогли решить кто сильнее... Оба пали в бою друг с другом.");
-            else if (_opponents[0].IsWarriorAlive())
-                Console.WriteLine($"Победил {_opponents[0].Name}!");
-            else
-                Console.WriteLine($"Победил {_opponents[1].Name}!");
-
-            Console.WriteLine("Нажмите любую клавишу...");
-            Console.ReadKey();
-            Console.Clear();
         }
 
         private Knight CreateKnight() => new Knight();
@@ -173,13 +175,48 @@ namespace Гладиаторские_бои
         private void AttackOpponent(Warrior warrior1, Warrior warrior2)
         {
             warrior1.Attack(warrior2);
+
+            WriteWarriorsStats();
+
             System.Threading.Thread.Sleep(1000);
+        }
+
+        private void TellFightResult()
+        {
+            if (_opponents[0].IsWarriorAlive() == false && _opponents[1].IsWarriorAlive() == false)
+                Console.WriteLine("Войны не смогли решить кто сильнее... Оба пали в бою друг с другом.");
+            else if (_opponents[0].IsWarriorAlive())
+                Console.WriteLine($"Победил {_opponents[0].Name}!");
+            else
+                Console.WriteLine($"Победил {_opponents[1].Name}!");
+        }
+
+        private void AskPressKey()
+        {
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private void WriteWarriorsStats()
+        {
+            int cursorPositionX = Console.CursorLeft;
+            int cursorPositionY = Console.CursorTop;
+
+            for (int i = 0; i < _opponents.Length; i++)
+            {
+                Console.SetCursorPosition(_statsPositionX, _statsPositionY + i);
+
+                _opponents[i].WriteStats();
+            }
+
+            Console.SetCursorPosition(cursorPositionX, cursorPositionY);
         }
     }
 
     interface ITakeDebuff
     {
-        void TakeDebuff(Action debuff, int debuffDuration);
+        void TakeDebuff(Func<int, int> debuff, int debuffDuration);
 
         void UseDebuff();
     }
@@ -194,34 +231,55 @@ namespace Гладиаторские_бои
         protected int Cooldown;
         protected int Counter = 0;
 
-        private Random _random = new Random();
         private int[] _healthStats = { 80, 100 };
         private int[] _armorStats = { 10, 20 };
         private int[] _damageStats = { 20, 30 };
         private int _armorDivider = 2;
+        private float _maxDamageAbsorptionPercent = 0.5f;
+
+        private ConsoleColor _healthColor = ConsoleColor.Red;
+        private ConsoleColor _armorColor = ConsoleColor.DarkGray;
+        private ConsoleColor _damageColor = ConsoleColor.DarkYellow;
 
         protected Warrior(string name, int cooldown)
         {
             Name = name;
-            Health = _random.Next(_healthStats[0], _healthStats[1]);
-            Armor = _random.Next(_armorStats[0], _armorStats[1]);
-            Damage = _random.Next(_damageStats[0], _damageStats[1]);
+            Health = UserUtilities.GenerateRandomNumber(_healthStats[1] + 1, _healthStats[0]);
+            Armor = UserUtilities.GenerateRandomNumber(_armorStats[1] + 1, _armorStats[0]);
+            Damage = UserUtilities.GenerateRandomNumber(_damageStats[1] + 1, _damageStats[0]);
             Cooldown = cooldown;
             Counter = cooldown;
         }
 
-        public virtual void TakeDamage(int damage) => Health -= damage - (Armor / _armorDivider);
+        public virtual void TakeDamage(int damage)
+        {
+            int armorAbsorption = (Armor / _armorDivider);
+            int maxDamageBlock = (int)(damage / _maxDamageAbsorptionPercent);
+
+            if (armorAbsorption > maxDamageBlock)
+                Health -= maxDamageBlock;
+            else
+                Health -= damage - (Armor / _armorDivider);
+        }
 
         public abstract void Attack(Warrior warrior);
 
         public bool IsWarriorAlive() => Health > 0;
+
+        public void WriteStats()
+        {
+            Console.Write(Name + ":");
+            UserUtilities.WriteColoredText(false, $" Здоровье - {Health} ", _healthColor);
+            UserUtilities.WriteColoredText(false, $"Броня - {Armor} ", _armorColor);
+            UserUtilities.WriteColoredText(false, $"Урон - {Damage}      ", _damageColor);
+        }
     }
 
     abstract class DebuffWarrior : Warrior, ITakeDebuff
     {
         protected int BaseDamage;
         protected int DebuffDuration;
-        protected Action Debuff = null;
+        protected Func<int, int> Debuff = null;
 
         public DebuffWarrior(string name, int cooldown) : base(name, cooldown) =>
             BaseDamage = Damage;
@@ -234,7 +292,7 @@ namespace Гладиаторские_бои
                 UseDebuff();
         }
 
-        public void TakeDebuff(Action debuff, int debuffDuration)
+        public void TakeDebuff(Func<int, int> debuff, int debuffDuration)
         {
             Debuff = debuff;
             DebuffDuration = debuffDuration;
@@ -243,7 +301,7 @@ namespace Гладиаторские_бои
 
         public void UseDebuff()
         {
-            Debuff?.Invoke();
+            Damage = Debuff?.Invoke(BaseDamage) ?? Damage;
 
             Debuff = null;
 
@@ -263,13 +321,13 @@ namespace Гладиаторские_бои
         private int _damageAbilityBonus;
         private int _healthBonus = 10;
         private int _armorBonus = 5;
-        private int _divider = 2;
+        private int _damageAbilityBonusDivider = 2;
 
         public Knight() : base("Рыцарь", 3)
         {
             Health += _healthBonus;
             Armor += _armorBonus;
-            _damageAbilityBonus = BaseDamage / _divider;
+            _damageAbilityBonus = BaseDamage / _damageAbilityBonusDivider;
         }
 
         public override void Attack(Warrior warrior)
@@ -283,7 +341,7 @@ namespace Гладиаторские_бои
             Console.WriteLine($"{Name} наносит {Damage} урона");
         }
 
-        public void CastDamageBuff()
+        private void CastDamageBuff()
         {
             if (Counter == Cooldown)
             {
@@ -302,19 +360,19 @@ namespace Гладиаторские_бои
         }
     }
 
-    class Mage : DebuffWarrior
+    class Mage : Warrior
     {
-        private Random _random = new Random();
         private List<Action<Warrior>> _spells = new List<Action<Warrior>>();
         private int _spellDebuffDuration = 3;
         private int _fireballDamage = 10;
         private int _fireballDamageBonus = 4;
-        private int _armorAbilityBonus = 3;
+        private int _armorAbilityBonus = 4;
         private int _armorBonus = -3;
         private float _debuffDamageMultiplier = 0.7f;
 
         public Mage() : base("Маг", 0)
         {
+            Damage = 0;
             Armor += _armorBonus;
             _spells.Add(CastDebuffOnEnemy);
             _spells.Add(CastFireball);
@@ -323,7 +381,7 @@ namespace Гладиаторские_бои
 
         public override void Attack(Warrior warrior)
         {
-            Action<Warrior> spell = _spells[_random.Next(_spells.Count)];
+            Action<Warrior> spell = _spells[UserUtilities.GenerateRandomNumber(_spells.Count)];
 
             spell.Invoke(warrior);
         }
@@ -348,14 +406,14 @@ namespace Гладиаторские_бои
             }
         }
 
-        private void ApplyDebuff() => Damage = (int)(Damage * _debuffDamageMultiplier);
+        private int ApplyDebuff(int damage) => (int)(damage * _debuffDamageMultiplier);
 
         private void CastFireball(Warrior warrior)
         {
             warrior.TakeDamage(_fireballDamage);
 
             _fireballDamage += _fireballDamageBonus;
-            Console.WriteLine($"{Name} кидает огненный шар.");
+            Console.WriteLine($"{Name} кидает огненный шар и наносит {_fireballDamage} урона.");
         }
 
         private void IncreaseArmor(Warrior warrior)
@@ -370,8 +428,7 @@ namespace Гладиаторские_бои
         private int _powerShotDamage = 10;
         private int _damageBonus = 5;
 
-        public Archer() : base("Лучник", 2) =>
-            Damage += _damageBonus;
+        public Archer() : base("Лучник", 2) => Damage += _damageBonus;
 
         public override void Attack(Warrior warrior)
         {
@@ -401,7 +458,7 @@ namespace Гладиаторские_бои
     class Paladin : Warrior
     {
         private int _maxHealth;
-        private int recoveryHealthQuantity = 8;
+        private int _recoveryHealthQuantity = 8;
         private int _healthBonus = 15;
         private int _armorBonus = 10;
         private int _damageBonus = 10;
@@ -439,13 +496,12 @@ namespace Гладиаторские_бои
         private void RecoverHealth()
         {
             if (Health < _maxHealth)
-                Health += recoveryHealthQuantity;
+                Health += _recoveryHealthQuantity;
         }
     }
 
     class Rogue : DebuffWarrior
     {
-        private Random _random = new Random();
         private int _maxPercent = 100;
         private int _dodgePercent = 30;
         private int _healthBonus = -10;
@@ -467,7 +523,7 @@ namespace Гладиаторские_бои
 
         public override void TakeDamage(int damage)
         {
-            int percent = _random.Next(_maxPercent + 1);
+            int percent = UserUtilities.GenerateRandomNumber(_maxPercent + 1);
 
             if (percent <= _dodgePercent)
             {
@@ -478,6 +534,26 @@ namespace Гладиаторские_бои
             {
                 base.TakeDamage(damage);
             }
+        }
+    }
+
+    static class UserUtilities
+    {
+        private static Random _random = new Random();
+
+        public static int GenerateRandomNumber(int maxRandomNumber, int minRandomNumber = 0) => _random.Next(minRandomNumber, maxRandomNumber);
+
+        public static void WriteColoredText(bool isCursorMoveNextLine, string text, ConsoleColor foregroundColor, ConsoleColor backgroundColor = ConsoleColor.Black)
+        {
+            Console.ForegroundColor = foregroundColor;
+            Console.BackgroundColor = backgroundColor;
+
+            if (isCursorMoveNextLine)
+                Console.WriteLine(text);
+            else
+                Console.Write(text);
+
+            Console.ResetColor();
         }
     }
 }
